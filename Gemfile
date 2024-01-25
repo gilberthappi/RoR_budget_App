4682a2ffdddea31edf81e19d1f0b4475e3f214c2
gilberthappi <gdushimimana6@gmail.com>
2024-01-25 13:59:54 +0200

    Add Linters configuration

--- Gemfile
+++ Gemfile
@@ -7,8 +7,8 @@ ruby '3.2.2'
 gem 'rails', '~> 7.0.4', '>= 7.0.4.2'
 
 # The original asset pipeline for Rails [https://github.com/rails/sprockets-rails]
-gem 'sprockets-rails'
 gem 'psych', '3.1.0'
+gem 'sprockets-rails'
 # Use postgresql as the database for Active Record
 gem 'pg', '~> 1.1'
 
--- test/controllers/splash_controller_test.rb
+++ test/controllers/splash_controller_test.rb
@@ -1,8 +1,8 @@
-require "test_helper"
+require 'test_helper'
 
 class SplashControllerTest < ActionDispatch::IntegrationTest
-    test "should get index" do
-        get splash_index_url
-        assert_response :success
-    end
+  test 'should get index' do
+    get splash_index_url
+    assert_response :success
+  end
 end
